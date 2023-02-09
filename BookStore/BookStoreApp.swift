bd71f598879d8d17ef2c0a44e89d31bc7b76f5df
David Nitzsche-Bell <david@zicount.com>
2023-02-09 13:01:04 +0100

    Initial Commit

---
+++ BookStore/BookStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreApp.swift
+//  BookStore
+//
+//  Created by Zicount on 09.02.23.
+//
+
+import SwiftUI
+
+@main
+struct BookStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
